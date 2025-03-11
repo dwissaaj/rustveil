24dd9865e4dcba21c3ecafed4f6f040eb8b42862
dwissaaj <dwisaji150@gmail.com>
2025-03-11 16:39:49 +0700

    move footer

---
+++ src/components/Footer.svelte
@@ -0,0 +1,16 @@
+<script>
+  
+  import { active } from "../routes/main/active.svelte";
+
+  
+</script>
+
+<footer class="absolute inset-x-0 bottom-0 p-2">
+    <div role="tablist" class="tabs tabs-lift bg-amber-50 p-2">
+      <button role="tab" class="tab" class:tab-active={$active === "edges"} onclick={() => active.set("edges")}>Edges</button>
+      <button role="tab" class="tab" class:tab-active={$active === "vertices"} onclick={() => active.set("vertices")}>Vertices</button>
+      <button role="tab" class="tab" class:tab-active={$active === "groups"} onclick={() => active.set("groups")}>Groups</button>
+      <button role="tab" class="tab" class:tab-active={$active === "metrics"} onclick={() => active.set("metrics")}>Metrics</button>
+      <button role="tab" class="tab" class:tab-active={$active === "nlp"} onclick={() => active.set("nlp")}>NLP</button>
+      </div>
+ </footer>
--- src/routes/main/+layout.svelte
+++ src/routes/main/+layout.svelte
@@ -1,8 +1,11 @@
 <script lang="ts" >
+    import Footer from '../../components/Footer.svelte';
+
   
-  import { active } from "./active.svelte";
+
+
   let { children } = $props();
-  const activeClass = "tab"
+
 </script>
 
 <div class="relative h-screen">
@@ -18,14 +21,6 @@
       </main>
       
       
-     <footer class="absolute inset-x-0 bottom-0 p-2 border-t-2">
-        <div role="tablist" class="tabs tabs-lift">
-          <button role="tab" class="tab" class:tab-active={$active === "edges"} onclick={() => active.set("edges")}>Edges</button>
-          <button role="tab" class="tab" class:tab-active={$active === "vertices"} onclick={() => active.set("vertices")}>Vertices</button>
-          <button role="tab" class="tab" class:tab-active={$active === "groups"} onclick={() => active.set("groups")}>Groups</button>
-          <button role="tab" class="tab" class:tab-active={$active === "metrics"} onclick={() => active.set("metrics")}>Metrics</button>
-          <button role="tab" class="tab" class:tab-active={$active === "nlp"} onclick={() => active.set("nlp")}>NLP</button>
-          </div>
-     </footer>
-      
+     
+      <Footer />
 </div>
