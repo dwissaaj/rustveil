c354b9bae6ed14f243b68564e7cc02c908fa33e9
dwissaaj <dwisaji150@gmail.com>
2025-08-09 20:46:39 +0700

    add rv folder

--- src-tauri/src/global/app_path/mod.rs
+++ src-tauri/src/global/app_path/mod.rs
@@ -1,15 +1,24 @@
-use tauri::Manager;
+use log::{error, info};
 use std::fs;
+use tauri::Manager;
 
-pub fn create_folder(app: tauri::AppHandle) -> String {
-    match app.path().app_local_data_dir() {
-        Ok(path) => {
-            // Create the folder (added this part)
-            if let Err(e) = fs::create_dir_all(&path) {
-                return format!("Failed to create folder: {}", e);
-            }
-            path.display().to_string() // Return path if successful
-        },
-        Err(e) => format!("Failed to get path: {}", e),
+pub fn create_folder_main_app(app: &tauri::App) -> String {
+    let public_dir = match app.handle().path().home_dir() {
+        Ok(path) => path,
+        Err(e) => {
+            error!("Failed to get public directory: {}", e);
+            return format!("Error: {}", e);
+        }
+    };
+    let full_path = public_dir.join("Rust Veil");
+    match fs::create_dir_all(&full_path) {
+        Ok(_) => {
+            info!("Successfully created directory at: {}", full_path.display());
+            format!("Created at: {}", full_path.display())
+        }
+        Err(e) => {
+            error!("Failed to create directory: {}", e);
+            format!("Error: {}", e)
+        }
     }
 }
