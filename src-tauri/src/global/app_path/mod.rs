12cc892983cbb5a20d1d3fd243d7250bff9a6ad1
dwissaaj <dwisaji150@gmail.com>
2025-08-12 20:28:31 +0700

    add state for filepath

--- src-tauri/src/global/app_path/mod.rs
+++ src-tauri/src/global/app_path/mod.rs
@@ -1,6 +1,20 @@
 use log::{error, info};
 use std::fs;
-use tauri::Manager;
+use tauri::{Builder, Window, WindowEvent, Manager};
+use std::sync::Mutex;
+#[derive(Default)]
+pub struct AppFolderPath {
+  pub file_url: String,
+}
+
+
+pub fn global_path_app(window: &Window, _event: &WindowEvent) -> String {
+
+    let app_handle = window.app_handle();
+    let state = app_handle.state::<Mutex<AppFolderPath>>();
+    let path = state.lock().unwrap().file_url.clone();
+    path
+}
 
 pub fn create_folder_main_app(app: &tauri::App) -> String {
     let public_dir = match app.handle().path().home_dir() {
@@ -13,6 +27,9 @@ pub fn create_folder_main_app(app: &tauri::App) -> String {
     let full_path = public_dir.join("Rust Veil");
     match fs::create_dir_all(&full_path) {
         Ok(_) => {
+            app.manage(Mutex::new(AppFolderPath {
+                file_url: full_path.display().to_string(),
+            }));
             info!("Successfully created directory at: {}", full_path.display());
             format!("Created at: {}", full_path.display())
         }
