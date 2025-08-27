ed57821c89c8bb752efe634cfde49ee1ae9e3b1f
dwissaaj <dwisaji150@gmail.com>
2025-08-27 18:37:36 +0700

    table component

--- app/work/(workgroup)/data/table/DataTable.tsx
+++ app/work/(workgroup)/data/table/DataTable.tsx
@@ -1,27 +1,28 @@
 import NoData from "@/components/workstation/data/NoData";
-import { Table, TableHeader, TableColumn, TableBody, TableRow, TableCell } from "@heroui/react";
+import { Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, CircularProgress } from "@heroui/react";
 
 interface DataTableProps {
-  data?: Record<string, any>[]; // Make data optional
+  data: Record<string, any>[];
+  isLoading?: boolean;
 }
 
-export default function DataTable({ data = [] }: DataTableProps) { 
- 
-  const columns = data && data.length > 0 ? Object.keys(data[0]) : [];
+export default function DataTable({ data, isLoading = false }: DataTableProps) {
+  // Get column names from the first data item
+  const columns = data.length > 0 ? Object.keys(data[0]) : [];
 
   return (
-    <Table isStriped isVirtualized aria-label="Data table">
+    <Table isVirtualized isStriped aria-label="Dynamic data table">
       <TableHeader>
         {columns.map((column) => (
           <TableColumn key={column}>{column}</TableColumn>
         ))}
       </TableHeader>
-      <TableBody emptyContent={<NoData />}>
+      <TableBody isLoading={isLoading} loadingContent={<CircularProgress />} emptyContent={<NoData />} >
         {data.map((row, index) => (
           <TableRow key={index}>
             {columns.map((column) => (
-              <TableCell key={column}>
-                {row[column]} 
+              <TableCell key={`${index}-${column}`}>
+                {row[column]?.toString() || ""}
               </TableCell>
             ))}
           </TableRow>
