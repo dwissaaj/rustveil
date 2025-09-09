c50c85683b22944965acf47cdd13c0f9074c1a19
dwissaaj <dwisaji150@gmail.com>
2025-09-09 15:05:53 +0700

    pie component

---
+++ app/work/(workgroup)/social_network/centrality/CentralityPie.tsx
@@ -0,0 +1,33 @@
+import { ResponsivePie } from "@nivo/pie";
+import { ColorSchemeId } from "@nivo/colors";
+import { FilterStateType } from "./state";
+export interface PieDatum {
+  id: string;
+  value: number;
+  label?: string;
+}
+export function CentralityPie({
+  data,
+  chartFilter,
+  height = "75vh",
+}: {
+  data: PieDatum[];
+  chartFilter: FilterStateType
+  height?: string;
+}) {
+  return (
+    <div className={`flex-1 h-[${height}]`}>
+      <ResponsivePie
+        data={data}
+        margin={{ top: 20, right: 20, bottom: 20, left: 20 }}
+        innerRadius={chartFilter.innerRadius}
+        padAngle={chartFilter.padAngle}
+        cornerRadius={chartFilter.cornerRadius}
+        activeOuterRadiusOffset={8}
+        arcLinkLabelsOffset={chartFilter.labelsOffset}
+        arcLinkLabelsTextOffset={chartFilter.textOffset}
+        colors={{ scheme: chartFilter.colorSchema as ColorSchemeId }}
+      />
+    </div>
+  );
+}
